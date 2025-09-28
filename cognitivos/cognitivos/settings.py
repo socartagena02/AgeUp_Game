94d273960901c8462b7478c42226b128a6660a13
SlayerBa23 <este.rodriguez@duocuc.cl>
2025-09-28 02:20:46 -0300

    Agrego modelos y migraciones para base de datos en MySQL

--- cognitivos/cognitivos/settings.py
+++ cognitivos/cognitivos/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+import pymysql  
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,14 +79,17 @@ WSGI_APPLICATION = 'cognitivos.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Geriatrico',     # nombre de tu base de datos en MySQL
+        'USER': 'admin',       # usuario MySQL
+        'PASSWORD': 'Root2025',  # contraseña MySQL
+        'HOST': 'database-2.cyjccqymugde.us-east-1.rds.amazonaws.com',        # o la IP/host de MySQL si está en AWS u otro servidor
+        'PORT': '3306',             # puerto por defecto de MySQL
     }
 }
+
 
 
 # Password validation
